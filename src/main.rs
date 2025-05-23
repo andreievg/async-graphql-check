ac4c162a89654cd0af6a44cc2bb0c4ed61858ff4
Andrei <andrei.evguenov@gmail.com>
2025-05-24 00:00:55 +1200

    Bind

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,7 @@ async fn main() -> std::io::Result<()> {
                     .to(graphiql_handler),
             )
     })
-    .bind("127.0.0.1:8000")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await
 }
